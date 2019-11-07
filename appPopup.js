4770e95a2241e986a1a0a8be2cf1aba0fbadc7c4
schappl1 <49418864+schappl1@users.noreply.github.com>
2019-11-07 15:34:48 -0500

    Add files via upload
    Template - added buttons form contact and homepage

---
+++ appPopup.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
+
+    window.open("https://webpages.uncc.edu/jcao6/Exercise/WebsiteProject/SEproject/SEproject.html");
+
+  }, false);
+}, false);
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('contact');
+  checkPageButton.addEventListener('click', function() {
+
+      window.open("https://webpages.uncc.edu/jcao6/Exercise/WebsiteProject/SEproject/contact_us.html");
+
+  }, false);
+}, false);
