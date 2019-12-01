f6451a4b6fd531ae2fe0806d40b5a392f1c89e79
schappl1 <49418864+schappl1@users.noreply.github.com>
2019-12-01 03:40:38 -0500

    Revamped master
    Added "quick report" button and "testHTML"

---
+++ ChromeExtension/src/appPopup.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
+
+    window.open("https://webpages.uncc.edu/jcao6/Exercise/WebsiteProject/SEproject/SEproject.html");
+
+  }, false);
+}, false);
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('contact');
+  checkPageButton.addEventListener('click', function() {
+
+      window.open("https://webpages.uncc.edu/jcao6/Exercise/WebsiteProject/SEproject/contact_us.html");
+
+  }, false);
+}, false);
+
+ 
+document.addEventListener('DOMContentLoaded', function() {
+	document.getElementById("quickRep1").style.display="none";
+
+  var qucikReportButton = document.getElementById('quickRep');
+  qucikReportButton.addEventListener('click', function() {
+
+ 
+				 document.getElementById("quickRep1").style.display="block";
+
+
+  }, false);
+}, false);
+
+
+
+
+
