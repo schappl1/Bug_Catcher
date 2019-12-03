23e2da46db1f89d12b46670b37704cb434cc9ae4
schappl1 <49418864+schappl1@users.noreply.github.com>
2019-12-03 02:32:34 -0500

    Updated URL

--- ChromeExtension/src/appPopup.js
+++ ChromeExtension/src/appPopup.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var checkPageButton = document.getElementById('checkPage');
   checkPageButton.addEventListener('click', function() {
 
-    window.open("https://webpages.uncc.edu/jcao6/Exercise/WebsiteProject/ContactUs/ContactUs.html);
+    window.open("https://webpages.uncc.edu/jcao6/Exercise/WebsiteProject/ContactUs/ContactUs.html");
 
   }, false);
 }, false);
@@ -13,20 +13,22 @@ document.addEventListener('DOMContentLoaded', function() {
   var checkPageButton = document.getElementById('contact');
   checkPageButton.addEventListener('click', function() {
 
-      window.open("https://webpages.uncc.edu/jcao6/Exercise/WebsiteProject/ContactUs/ContactUs.html);
+      window.open("https://webpages.uncc.edu/jcao6/Exercise/WebsiteProject/ContactUs/ContactUs.html");
 
   }, false);
 }, false);
 
+ 
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("quickRep1").style.display="none";
+	document.getElementById("quickRep1").style.display="none";
 
   var qucikReportButton = document.getElementById('quickRep');
   qucikReportButton.addEventListener('click', function() {
 
  
-  document.getElementById("quickRep1").style.display="block";
+				 document.getElementById("quickRep1").style.display="block";
 
 
   }, false);
 }, false);
+
